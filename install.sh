75aceceff65729f4722d2ace013aa27449502a33
David P. Chassin <dchassin@slac.stanford.edu>
2021-01-21 15:35:04 -1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "#$(which python3)" > /usr/local/bin/numpy
+echo "#!$(which python3)" > /usr/local/bin/numpy
 curl https://raw.githubusercontent.com/dchassin/numpy-cli/master/src/numpy.py >>/usr/local/bin/numpy
 chmod +x /usr/local/bin/numpy
